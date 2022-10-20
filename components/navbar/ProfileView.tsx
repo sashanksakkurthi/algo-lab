215912fe306944a2ba8691695111176c247cb776
sakkurthi sashank <sakkurthisashank@gmail.com>
2022-10-20 18:33:52 +0530

    feat: code editor window , fix: navigation

--- components/navbar/ProfileView.tsx
+++ components/navbar/ProfileView.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import { MdNotificationsNone } from "react-icons/md";
+import { Menu } from "@headlessui/react";
+import { Fragment } from "react";
 
 const ProfileView: React.FC<{}> = () => {
-  const [isProfileOpen, setIsProfileOpen] = React.useState(false);
-  const profileLinks = ["Profile", "Settings", "Contact Us", "Help"];
+  const profileLinks = ["Contact Us", "Help"];
   return (
-    <div className="flex items-center md:space-x-4">
+    <div className="flex items-center">
       <button className="relative rounded-full p-2 hover:bg-gray-100">
         <div className="text-gray-700">
           <MdNotificationsNone size={27} />
@@ -14,34 +15,36 @@ const ProfileView: React.FC<{}> = () => {
           0
         </div>
       </button>
-      <button
-        className="flex flex-col items-center"
-        onMouseEnter={() => setIsProfileOpen(!isProfileOpen)}
-      >
-        <picture>
-          <img
-            className="h-8 w-8 rounded-full"
-            src="https://bit.ly/dan-abramov"
-            alt=""
-          />
-        </picture>
-        <div className="font-inter text-xs font-medium">Sakkurthi</div>
-      </button>
-      {isProfileOpen ? (
-        <div
-          className="absolute top-[58px] right-1 z-10 flex h-40 w-40 flex-col space-y-1 rounded-md border border-gray-300 bg-white p-2"
-          onMouseLeave={() => setIsProfileOpen(!isProfileOpen)}
-        >
-          {profileLinks.map((link) => (
-            <button
-              key={link}
-              className="rounded-md py-1 font-inter font-medium text-gray-700 hover:bg-blue-600 hover:text-white"
-            >
-              {link}
-            </button>
-          ))}
-        </div>
-      ) : null}
+      <Menu>
+        <Menu.Button className={"flex flex-col items-center px-4"}>
+          <>
+            <picture>
+              <img
+                className="h-7 w-7 rounded-full"
+                src="https://bit.ly/dan-abramov"
+                alt=""
+              />
+            </picture>
+            <div className="font-inter text-xs font-medium">Sakkurthi</div>
+          </>
+        </Menu.Button>
+        <Menu.Items>
+          <div className="absolute top-[3.25rem] right-1 z-10 flex w-36 flex-col space-y-1 rounded-md border border-gray-300 bg-white p-2">
+            {profileLinks.map((link) => (
+              <Menu.Item key={link} as={Fragment}>
+                {({ active }) => (
+                  <button
+                    key={link}
+                    className="rounded-md py-1 font-inter font-medium text-gray-700 hover:bg-blue-600 hover:text-white"
+                  >
+                    {link}
+                  </button>
+                )}
+              </Menu.Item>
+            ))}
+          </div>
+        </Menu.Items>
+      </Menu>
     </div>
   );
 };
